f64e26a3bf80ff9e4cff769f5a88fce31e236321
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-23 18:29:07 +0200

    add DEXTER module

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+var _ = require('lodash'),
+  google = require('googleapis'),
+  OAuth2 = google.auth.OAuth2;
+
 module.exports = {
+    checkAuthOptions: function (step, dexter) {
+
+        if(!step.input('userId').first()) {
+
+            this.fail('A userId input variable is required for this module');
+        }
+
+        if(!dexter.environment('access_token')) {
+
+            this.fail('A access_token environment variable is required for this module');
+        }
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +23,32 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        this.checkAuthOptions(step, dexter);
+
+        var oauth2Client = new OAuth2();
+        oauth2Client.setCredentials({access_token: dexter.environment('access_token'), refresh_token: dexter.environment('refresh_token')});
+
+        google.options({ auth: oauth2Client });
+        console.log({
+            auth: oauth2Client,
+            userId: step.input('userId', null).first(),
+            resource: {
+                name: step.input('name').first(),
+                labelListVisibility: step.input('removeLabelIds').first(),
+                messageListVisibility: step.input('messageListVisibility').first()
+            }});
+        google.gmail('v1').users.labels.create({
+            auth: oauth2Client,
+            userId: step.input('userId', null).first(),
+            resource: {
+                name: step.input('name').first(),
+                labelListVisibility: step.input('labelListVisibility').first(),
+                messageListVisibility: step.input('messageListVisibility').first()
+            }}, function (err, message) {
+
+            err? this.fail(err) : this.complete(message);
+        }.bind(this));
+
     }
 };
